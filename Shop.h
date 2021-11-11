fa41a10f0631160937ec96afb047f0c65ad46d53
srpg15 <73872542+srpg15@users.noreply.github.com>
2021-11-11 15:10:26 -0700

    Add files via upload
    Adding to main

---
+++ Shop.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Shop{
+
+    public:
+    Shop(int newPlayerMoney);
+    int getBalance();
+    void setBalance(int newBalance);
+    int getPrice();
+    void setPrice(int newPrice);
+    string getItemName();
+    void setItemName(string newName);
+    bool itemBought(string itemName);
+    void populateInventory(string shopFile);
+    int printMenu(string shopInventory[], int playerMoney);
+
+
+    private:
+    int itemPrice;
+    int playerMoney;
+    int newPlayerMoney;
+    string shopFile;
+    string itemFile;
+    string itemName;
+    string newItem;
+    string shopInventory[placeholder];
+};
