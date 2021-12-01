62345cc24572ef9fab804ca95daac54f854fc3f2
Miles Silveria <40589813+f1fty28ty@users.noreply.github.com>
2021-12-01 15:03:05 -0700

    Update Map.h

--- Map.h
+++ Map.h
@@ -10,20 +10,19 @@ using namespace std;
 class Map
 {
      private:
-          static const int num_rows = 5;
-          static const int num_cols = 9;
-          static const int num_npcs = 3;
-          static const int num_boss = 1;
+          static const int num_rows = 6;
+          static const int num_cols = 10;
+          static const int num_Shops = 3;
+          static const int num_Levels = 6;
 
           int playerPosition[2];
-          int bestBuyPosition[2];
-          int npcPositions[num_npcs][2];
-          int bossPositions[num_boss][2];
+          int levelPosition[num_Levels][2];
+          int shopPositions[num_Shops][2];
           char mapData[num_rows][num_cols];
 
-          int npc_count;
-          int boss_count;
-          bool best_buy_on_map;
+          int shop_count;
+          int level_count;
+          bool level_on_map;
 
      public :
           Map();
@@ -33,28 +32,24 @@ class Map
           // getters
           int getPlayerRowPosition();
           int getPlayerColPosition();
-          int getNPCCount();
-          int getBossCount();
+          int getShopCount();
+          int getLevelCount();
 
           // setters
           void setPlayerRowPosition(int);
           void setPlayerColPosition(int);
-          void setNPCCount(int);
-          void setBossCount(int);
+          void setShopCount(int);
+          void setLevelCount(int);
 
-          bool spawnNPC(int, int);
-          bool spawnBestBuy(int, int);
-          bool spawnBoss(int, int);
+          bool spawnShop(int, int);
+          bool spawnLevel(int, int);
 
           void displayMap();
           void displayMoves();  
           bool executeMove(char);
 
-          bool isBestBuyLocation();
-          bool isNPCLocation();
-          bool isBossLocation();
-
-          bool isBestBuyOnMap();
+          bool isLevelLocation();
+          bool isShopLocation();
 };
  
 #endif
