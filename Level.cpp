590458717881885624155159c23ad65345c5cf68
Miles Silveria <40589813+f1fty28ty@users.noreply.github.com>
2021-12-01 15:00:15 -0700

    Update Level.cpp

--- Level.cpp
+++ Level.cpp
@@ -1,6 +1,58 @@
 #include "Level.h"
 
-Level::Mobs::Enemy(){
+Level::Level(){
+    lvlname = "Beginnings";
+    numMobs = 4;
+    lvlmulti = 1;
+    lvlMobs[0].setName("Green Slime");
+    lvlMobs[1].setName("Blue Slime");
+    lvlMobs[2].setName("Red Slime");
+    lvlMobs[3].setName("Gold Slime");
+    for(int i = 0; i < numMobs; i++){
+        lvlMobs[i].setXp(10*lvlmulti);
+        lvlMobs[i].setMoney(10*lvlmulti);
+        lvlMobs[i].setDmg(10*lvlmulti);
+    }
+}
+Level::Level(string newName,int newNumMobs,Enemy newMobs[],int newDif){
+    Level();
+    lvlmulti = (1 * 10 * newDif);
+    lvlname = newName;
+    numMobs = 4;
+    if(newNumMobs > numMobs){
+        for(int i = 0;i < numMobs;i++){
+            lvlMobs[i] = Enemy();
+            lvlMobs[i] = newMobs[i];
+            lvlMobs[i].setXp(10*lvlmulti);
+            lvlMobs[i].setMoney(10*lvlmulti);
+            lvlMobs[i].setDmg(10*lvlmulti);
+        }
+    }else if(newNumMobs < numMobs){
+        for(int i = 0; i < newNumMobs; i++){
+            lvlMobs[i] = Enemy();
+            lvlMobs[i] = newMobs[i];
+            lvlMobs[i].setXp(10*lvlmulti);
+            lvlMobs[i].setMoney(10*lvlmulti);
+            lvlMobs[i].setDmg(10*lvlmulti);
+        }
+        for(int i = newNumMobs;i < numMobs;i++){
+            lvlMobs[i] = Enemy();
+            lvlMobs[i].setXp(10*lvlmulti);
+            lvlMobs[i].setMoney(10*lvlmulti);
+            lvlMobs[i].setDmg(10*lvlmulti);
+        }
+    }else{
+        for(int i = 0;i < numMobs;i++){
+            lvlMobs[i] = Enemy();
+            lvlMobs[i] = newMobs[i];
+            lvlMobs[i].setXp(10*lvlmulti);
+            lvlMobs[i].setMoney(10*lvlmulti);
+            lvlMobs[i].setDmg(10*lvlmulti);
+        }
+    }
+}
+
+Level::Mobs::Mobs(){
     name = "Slime";
     dmg = 10;
     money = 10;
